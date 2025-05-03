ea478630578644a834db13380fc20c0ff2316e31
aapl00 <aapl00@proton.me>
2025-05-03 10:12:12 -0400

    Add ChangePasswordSerializers

--- user/serilizers.py
+++ user/serilizers.py
@@ -22,3 +22,21 @@ class UserSerializer(serializers.Serializer):
             password = validated_data['password']
         )
         return user
+
+class ChangePasswordSerializer(serializers.Serializer):
+    current_password = serializers.CharField(write_only=True, required=True, style={'input_type': 'password'})
+    new_password = serializers.CharField(write_only=True, required=True, style={'input_type': 'password'})
+
+    def validate_current_password(self, value):
+        if not self.context['request'].user.check_password(value):
+            raise serializers.ValidationError('Incorrect password')
+        return value
+
+    def validate_new_password(self, value):
+        validate_password(value, user=self.context['request'].user)
+        return value
+
+    def update(self, instance, validated_data):
+        instance.set_password(validated_data['new_password'])
+        instance.save()
+        return instance
