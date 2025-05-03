a587e027567ffc85f3068dd816a3f49ff17795e3
aapl00 <aapl00@proton.me>
2025-05-03 10:12:34 -0400

    Add ChangePasswordView

--- user/views.py
+++ user/views.py
@@ -1,9 +1,16 @@
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated, AllowAny
-from .serilizers import UserSerializer
+from .serilizers import UserSerializer, ChangePasswordSerializer
 from .models import CustomUser
 
 class CreateUserView(generics.CreateAPIView):
     permission_classes = [AllowAny]
     serializer_class = UserSerializer
     queryset = CustomUser.objects.all()
+
+class ChangePasswordView(generics.UpdateAPIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = ChangePasswordSerializer
+
+    def get_object(self):
+        return self.request.user
