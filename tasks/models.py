9ce1bdea2226f46a9cc267d2dd2d772bcb503a4e
aapl00 <aapl00@proton.me>
2025-05-05 15:57:24 -0400

    Add field user as foreign key to Task model

--- tasks/models.py
+++ tasks/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.conf import settings
+
 
 class Task(models.Model):
     title = models.CharField(max_length=200)
@@ -6,6 +8,7 @@ class Task(models.Model):
     completed = models.BooleanField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='tasks')
 
     def __str__(self):
         return self.title
