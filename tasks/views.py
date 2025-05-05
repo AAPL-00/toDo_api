d2d28760ee86ccd4490d662fc1d4dc9c5ca177a9
aapl00 <aapl00@proton.me>
2025-05-05 15:56:40 -0400

    Add CreateTaskView and RetrieveUpdateTask

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Task
+from .serializers import TaskSerializer
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+class CreateTaskView(generics.CreateAPIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = TaskSerializer
+    queryset = Task.objects.all()
+
+class RetrieveUpdateTask(generics.RetrieveUpdateAPIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = TaskSerializer
+    queryset = Task.objects.all()
